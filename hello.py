fab3a41f01acbb7ac49c9ddf23c37185a8564df0
Théodore <theodore.follet@gmail.com>
2020-11-08 21:45:29 +0100

    Adding lot of functions, to can get back all hero for a user directly from the database, to can get and contruct the questBook directly from the database, adding some functions for the instiantion of the different heroes of a user, the different quests, if we decide to make the possibility to add a quest, the script will directly add the quest in the database, update the database too (I mean the schema.sql file)

--- hello.py
+++ hello.py
@@ -20,18 +20,39 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 @app.route('/', methods=["GET", "POST"]) #Route for the login/register/game page
 def Login():
     """The login function will try to connect the user, if the username doesn't exist in the database, that will redirect the user to the registerPage.html."""
+    theQuestBook = createTheQuestBook()
     pageToLoad = "loginPage.html" #Default value of the pageToLoad
     if(request.method == 'POST'):
-        #Make an instance of the user
-        #Call the User constructor
-        theConnectedUser = User(request.form['username'],request.form['password']) #Give the parameters that the function __init__ need
-        pageToLoad = session["pageToLoad"]
+        theConnectedUser = User.login(request.form['username'],request.form['password']) #Initialize the user with the username and the password
+        session["username"] = theConnectedUser.getUserUsername()
+        pageToLoad = session["pageToLoad"] #Refresh the pageToLoad variable with the session pageToLoad var
+        theQuestBook = createTheQuestBook() #Initialise the quest book
+        
+        #theNewHero = Hero("GetRekt",1,"Dague",15,"Manger",0,numQuest = 0,numStep = 0)
+        #theConnectedUser.addAHeroToTheList(theNewHero)
+
+
+        #Only for Logs/Debug
+        print("All the quest of the quest book")
+        for aQuest in theQuestBook.getAllQuests():
+            print("Quest id : "+ str(aQuest.idOfTheQuest))
+            print(aQuest.toString())
+
+        
     else: #That mean the request.method is egal to GET
         if(session.get("username") is not None): #Check if the user is already connect or not
             pageToLoad = 'gamePage.html' #Set the pageToLoad variable to gamePage.html
         else:
             pageToLoad = 'loginPage.html' #Set the pageToLoad variable to loginPage.html
+    if(pageToLoad == "gamePage.html"):
+        theHeroes = theConnectedUser.getTheListOfHeroes()
+        return render_template(pageToLoad, theHeroes=[theHeroes])
     return render_template(pageToLoad) #Rendering the template with the variable pageToLoad
+
+def createTheQuestBook():
+    theQuestBook = QuestBook() #Initiliaze the quest book
+    return theQuestBook
+
 
 @app.route('/deleteAccount', methods=["POST"]) #Route for deleting the current account
 def deleteAccount():
@@ -50,7 +71,6 @@ def deleteAccount():
 def logout():
     """The logout function will destruct everything in the session and then redirect to the main route."""
     for value in session.copy():
-        usernameOfTheUserBeforeLogout = session["username"]
         session.pop(value, None) #Destruct all values in the session
 
     return redirect("/") #Redirect to the main route
@@ -118,40 +138,61 @@ def checkTheValidityOfThePassword(thePasswordToCheck, hashAndSaltFromTheDatabase
 
 #Class User
 class User:
-    def __init__(self,username,password):
-        self.username = username #Set the usename of the User object
-        self.password = password #Set the password of the User object
-        self.connected = False
-        self.listOfHero = [] #Initialize the list as empty
-
-        if(session.get("username") is None): #If the username is not in the session, yet
-            #To initialize the user we have to check if the username and password is valid
-            myDatabaseAccess = get_db() #Get the database in a variable, we are going to use this variable later to select, insert, update values
-            resultatRequest = myDatabaseAccess.execute("SELECT password,id FROM user WHERE username = '%s'" % self.username).fetchone() #Get the password and the id from the database, password and id that match with the username that the user give in the form
-            if(resultatRequest is not None): #Check if the result of the SQL request is not egal to none
-                passwordAndHashReturn = resultatRequest[0]
-                self.id = resultatRequest[1] #Set the User id in the object variable
-                if(passwordAndHashReturn !=""): #If the result is not null
-                    passwordAreTheSame = checkTheValidityOfThePassword(self.password,passwordAndHashReturn)
-                if(passwordAreTheSame): #If the both passwords matched
-                    print(f"The user : {request.form['username']} is now connected") #Useful to make log in the console of the flask "server" and to debug
-                    self.connected = True #Set the connected var to True
-                    session["username"] = self.username #Save in the session variable the username of the user
-                    session["pageToLoad"] = "gamePage.html" #Set the pageToLoad variable session to gamePage.html
-                else:
-                    session["pageToLoad"] = "loginPage.html" #Set the pageToLoad variable session to loginPage.html
+    @staticmethod
+    def login(username,password):
+        #To initialize the user we have to check if the username and password is valid
+        myDatabaseAccess = get_db() #Get the database in a variable, we are going to use this variable later to select, insert, update values
+        resultatRequest = myDatabaseAccess.execute("SELECT password,idUser FROM user WHERE username = '%s'" % username).fetchone() #Get the password and the id from the database, password and id that match with the username that the user give in the form
+        if(resultatRequest is not None): #Check if the result of the SQL request is not egal to none
+            passwordAndHashReturn = resultatRequest[0]
+            if(passwordAndHashReturn !=""): #If the result is not null
+                passwordAreTheSame = checkTheValidityOfThePassword(password,passwordAndHashReturn)
+            if(passwordAreTheSame): #If the both passwords matched
+                print(f"The user : {username} is now connected") #Useful to make log in the console of the flask "server" and to debug
+                session["pageToLoad"] = "gamePage.html" #Set the pageToLoad variable session to gamePage.html
+                return User(resultatRequest[1],username) #Return the user we just create cause the login informations are correct
             else:
-                session["pageToLoad"] = "registerPage.html" #Set the pageToLoad variable session to registerPage.html
-        else: #That mean the username if set in the session
-            print(f"The user : {self.username} is already connected") ##Useful to make log in the console of the flask "server" and to debug
-            session["pageToLoad"] = "gamePage.html" #Set the pageToLoad variable session to gamePage.html
+                session["pageToLoad"] = "loginPage.html" #Set the pageToLoad variable session to loginPage.html
+        else:
+            session["pageToLoad"] = "registerPage.html" #Set the pageToLoad variable session to registerPage.html
+        return None #Return None because the user informations give in the form are incorrect
 
-    def __getUserUsername__(self):
+    def __init__(self,id,username):
+        self.id = id #Set the id of the User object
+        self.username = username #Set the usename of the User object
+        self.listOfHero = list() #Initialize the list as empty
+        self.setTheListOfHeroFromTheDatabase() #Call the function to get the database of the current hero from the database
+    
+    def addAHero(self,theHeroToAdd):
+        self.listOfHero.append(theHeroToAdd)
+    
+    def showAllHeroes(self):
+        for aHero in self.listOfHero:
+            print(aHero.toString())
+
+    def getUserUsername(self):
         return self.username
 
-    def __setTheList__(self):
-        pass
+    def setTheListOfHeroFromTheDatabase(self): #This function will initialize the hero of the current, by getting the informations from the database
+        myDatabaseAccess = get_db() #Get the database in a variable, we are going to use this variable later to select, insert, update values
+        resultatOfTheRequest = myDatabaseAccess.execute("SELECT nameOfTheHero, lvl, weapon, armor, passive, sexe, numQuest, numStep FROM hero INNER JOIN user ON hero.idUser = user.idUser WHERE username = '%s'" % self.username) #Get the the list of hero from the database
+        if(resultatOfTheRequest is not None):
+            for resultRow in resultatOfTheRequest:
+                aHero = Hero(resultRow[0],resultRow[1],resultRow[2],resultRow[3],resultRow[4],resultRow[5],resultRow[6],resultRow[7]) #Create the intance of the hero
+                self.addAHero(aHero) #Add the hero to the list of hero of the user
+    
+    def addAHeroToTheList(self,theHeroToAdd): #This function have to be called when you want to add a new hero and add it in the database at the same time
+        self.listOfHero.append(theHeroToAdd) #Add it in the listOfHero attribut
+        #Add the hero in the database
+        myDatabaseAccess = get_db() #Get the database in a variable, we are going to use this variable later to select, insert, update values
+        infosHero = [theHeroToAdd.name,theHeroToAdd.lvl,theHeroToAdd.weapon,theHeroToAdd.armor,theHeroToAdd.passive,theHeroToAdd.sexe,self.id] #Initialise the infosHero var, we are going to give to the execute function to insert the hero in the database
+        resultatRequest = myDatabaseAccess.execute("INSERT INTO hero(nameOfTheHero,lvl,weapon,armor,passive,sexe,idUser) VALUES(?,?,?,?,?,?,?)", infosHero) #Insert into the database the hero
+        resultOfTheCommit = myDatabaseAccess.commit()
+        print(resultOfTheCommit) #Save the change in the database.db file
 
+    def getTheListOfHeroes(self):
+        return self.listOfHero
+    
 #Class Entity
 class Entity:
     def __init__(self,name,lvl,weapon,armor,passive):
@@ -163,10 +204,20 @@ class Entity:
 
 #Class Hero (extend from Entity)
 class Hero(Entity):
-    def __init__(self,name,lvl,weapon,armor,passive,questbook,sexe):
-        Hero.init(name,lvl,weapon,armor,passive)
-        self.questbook = questbook
+    def __init__(self,name,lvl,weapon,armor,passive,sexe,numQuest = 0,numStep = 0):
+        Entity.__init__(self,name,lvl,weapon,armor,passive)
         self.sexe = sexe
+        self.numQuest = numQuest
+        self.numStep = numStep
+    
+    def getTheNameOfTheHero(self):
+        return self.name
+
+    def toString(self):
+        return "Je m'appelle : " + self.name + " Je suis niveau : " + str(self.lvl) + " Je suis équipé avec : " + self.weapon + " J'ai : " + str(self.armor) + " d'armure" 
+    
+    def getNumQuest(self):
+        return self.numQuest
 
 #Class Monster (extend from Entity)
 class Monster(Entity):
@@ -176,36 +227,100 @@ class Monster(Entity):
 
 #Class QuestBook (contain some quests)
 class QuestBook:
-    def __init__(self,type,quests):
-        self.type = type
+    def __init__(self,quests = None):
         if(not quests):
-            self.quests = [] #Initialize the list as empty
+            self.quests = list() #Initialize the list as empty
         else:
             self.quests = quests
+        self.initilaliseTheQuestsFromTheDatabase()
 
-    def addQuests(self,aQuestToAdd):
-        self.quests.append(aQuestToAdd) #That will add the quest to the end of the list
+    def addAQuest(self,aQuestToAdd):
+        self.quests.append(aQuestToAdd)
+
+    def getASpecificStepOfAQuest(self,questNumber,stepNumber):
+        for aQuest in self.quests:
+            if(aQuest.id == questNumber):
+                for aStep in aQuest.steps:
+                    if(aStep.id == stepNumber):
+                        return aStep.text
+    
+    def getAllQuests(self):
+        return self.quests
+
+    def initilaliseTheQuestsFromTheDatabase(self):
+        myDatabaseAccess = get_db() #Get the database in a variable
+        resultatOfTheRequest = myDatabaseAccess.execute("SELECT * FROM quest") #Get the the list of quests from the database
+        if(resultatOfTheRequest is not None):
+            for resultRow in resultatOfTheRequest:
+                print("Id of the quest : "+ str(resultRow[0]) + " name of the quest : " + resultRow[1] )
+                aQuestToAdd = Quest(resultRow[0],resultRow[1]) #Create the intance of the quest
+                self.addAQuest(aQuestToAdd) #Add the quest to the list of quest of the QuestBook
 
 #Class Quest (contain some steps)
 class Quest:
-    def __init__(self,id,steps):
-        self.id = id
+    def __init__(self,idOfTheQuest,nameOfTheQuest,steps = None):
+        self.idOfTheQuest = idOfTheQuest
+        self.nameOfTheQuest = nameOfTheQuest
         if(not steps):
-            self.steps = [] #Initialize the list as empty
+            self.steps = list() #Initialize the list as empty
         else:
             self.steps = steps
 
-    def addSteps(self,aStepToAdd):
+        self.initilaliseTheStepsFromTheDatabase() #Get all the steps store in the database
+
+    @staticmethod
+    def createATotallyNewQuest(nameOfTheQuest,steps = None):
+        #We have to get the last step id for this specific quest
+        myDatabaseAccess = get_db() #Get the database in a variable
+        resultMaxQuestIdRequest = myDatabaseAccess.execute("SELECT MAX(questId) FROM quest").fetchone() #Get the maximum questId
+        theMaxQuestId = resultMaxQuestIdRequest[0]+1 #Add +1 at max questId
+        infosQuest = [theMaxQuestId,nameOfTheQuest]
+        resultatOfTheInsertRequest = myDatabaseAccess.execute("INSERT INTO quest(questId,nameOfTheQuest) VALUES(?,?)", infosQuest) #Insert into the database the quest
+        resultatOfTheInsertRequest = myDatabaseAccess.commit() #Save the change in the database.db file
+        theNewQuest = Quest(nameOfTheQuest,steps = None) #Create a quest object
+        return theNewQuest #Return this quest object
+
+    def initilaliseTheStepsFromTheDatabase(self):
+        myDatabaseAccess = get_db() #Get the database in a variable
+        resultatOfTheRequest = myDatabaseAccess.execute("SELECT stepId,textOfTheStep,step.questId FROM step INNER JOIN quest ON step.questId = quest.questId WHERE step.questId = '%s'" % self.idOfTheQuest) #Get the the list of step from the database for a specifique quest
+        if(resultatOfTheRequest is not None):
+            for resultRow in resultatOfTheRequest:
+                aStepToAdd = Step(resultRow[0],resultRow[1],resultRow[2]) #Create the intance of the step
+                self.addStep(aStepToAdd) #Add the step to the list of step of the quest
+        
+
+    def addStep(self,aStepToAdd):
         self.steps.append(aStepToAdd) #That will add the step to the end of the list
+
+    def toString(self): #Print all the steps of a quest | Use only for debug during development
+        for aStep in self.steps:
+            print(aStep.toString())
 
 #Class Step
 class Step:
-    def __init__(self,id,text):
-        self.id = id
-        self.text = text
+    def __init__(self,stepNumber,textOfTheStep,idOfTheQuest):
+        self.stepNumber = stepNumber
+        self.text = textOfTheStep
+        self.idOfTheQuest = idOfTheQuest
 
     def setTheStepText(self,text):
         self.text = text
 
     def getTheStepText(self):
         return self.text
+    
+    def toString(self): #Return the text of a step | Use only for debug during development
+        return "Step number : " + str(self.stepNumber) + " you have to do : " + self.text
+    
+    @staticmethod
+    def createATotallyNewStep(idOfTheQuest,textOfTheStep):
+
+        #We have to get the last step id for this specific quest
+        myDatabaseAccess = get_db() #Get the database in a variable
+        resultMaxStepNumberRequest = myDatabaseAccess.execute("SELECT MAX(stepNumber) FROM step INNER JOIN quest ON step.questId = quest.questId WHERE quest.questId = '%s'" % idOfTheQuest).fetchone() #Get the maximum stepNumber for a specifique quest
+        theMaxStepNumber = resultMaxStepNumberRequest[0]+1 #Add +1 at the last stepNumber
+        infosStep = [idOfTheQuest,textOfTheStep,theMaxStepNumber]
+        resultatOfTheInsertRequest = myDatabaseAccess.execute("INSERT INTO quest(questId,nameOfTheQuest,stepNumber) VALUES(?,?,?)", infosStep) #Insert into the database the step
+        resultatOfTheInsertRequest = myDatabaseAccess.commit() #Save the change in the database.db file
+        theNewStep = Step(theMaxStepNumber,textOfTheStep,idOfTheQuest)
+        return theNewStep
--- schema.sql
+++ schema.sql
@@ -1,11 +1,55 @@
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS hero;
+DROP TABLE IF EXISTS questBook;
+DROP TABLE IF EXISTS quest;
+DROP TABLE IF EXISTS step;
 
 CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  idUser INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
+);
+
+CREATE TABLE hero (
+  idHero INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  nameOfTheHero TEXT UNIQUE NOT NULL,
+  lvl INT DEFAULT 1,
+  weapon TEXT DEFAULT "Hands",
+  armor INT DEFAULT 0,
+  passive TEXT,
+  sexe BOOLEAN DEFAULT 0,
+  idUser INT,
+  numQuest INT DEFAULT 0,
+  numStep INT DEFAULT 0,
+  FOREIGN KEY (idUser) REFERENCES user(idUser)
+);
+
+CREATE TABLE quest (
+  questId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  nameOfTheQuest TEXT
+);
+
+CREATE TABLE step (
+  stepId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  stepNumber INT NOT NULL,
+  textOfTheStep TEXT DEFAULT "Text of the step isn't set yet",
+  questId INT,
+  FOREIGN KEY (questId) REFERENCES quest(questId)
 );
 
 INSERT INTO user(username,password)
 VALUES
 ("theodore","$2b$12$wodx1pJqggHk7liJkwT22uLDMWxHBvp1lRGje.Ic05TISPPP5sRtm");
+
+INSERT INTO hero(nameOfTheHero,lvl,weapon,armor,passive,sexe,idUser)
+VALUES
+("My first hero", 1,"Epee",10,"Nothing",0,1);
+
+
+INSERT INTO quest(nameOfTheQuest)
+VALUES
+("The name of the quest");
+
+INSERT INTO step(textOfTheStep,questId,stepNumber)
+VALUES
+("The step text ...",1,0);
